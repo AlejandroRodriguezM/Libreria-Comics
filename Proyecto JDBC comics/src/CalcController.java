e822af23bd68bac1a2fd2a1e4dd6244e3dff2b8d
alejandrorm <alejandrorm@alejandrorm>
2022-05-30 23:35:50 +0200

    Version Inicial no funcional

---
+++ Proyecto JDBC comics/src/CalcController.java
@@ -0,0 +1,12 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class CalcController {
+
+}
---
+++ Proyecto JDBC comics/src/Principal.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Principal extends Application{
+	
+	//Metodo que permite la ejecutcion de la aplicacion.
+		public void start(Stage primaryStage) throws IOException 
+		{
+			Parent root = FXMLLoader.load(getClass().getResource("gui.fxml")); //Obtenemos el diseño en la ruta y fichero ya creado.
+			primaryStage.setScene(new Scene(root)); //Creamos una escena para poder usar la aplicacion.
+			primaryStage.setTitle("Calculadora simple opcional"); //Titulo de la aplicacion.
+			primaryStage.show(); //Mostramos la aplicacion-
+
+		}
+		public static void main(String[] args) 
+		{
+			launch(args);
+		}
+
+}
