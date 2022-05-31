e7e1d2d5502a920884548446b0865d765b63cfd5
alejandrorm <alejandrorm@alejandrorm>
2022-06-01 00:48:05 +0200

    Añadido campo de contraseña no visible

--- Proyecto JDBC comics/src/Funcionamiento/Controller.java
+++ Proyecto JDBC comics/src/Funcionamiento/Controller.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -56,8 +57,8 @@ public class Controller {
 	@FXML
 	private TextField nombreVariante;
 
-	@FXML
-	private TextField contraBBDD;
+    @FXML
+    private PasswordField contraBBDD;
 
 	@FXML
 	private TextField numeroComic;
