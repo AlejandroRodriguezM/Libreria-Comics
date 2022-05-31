0292b183bcb22d614cb306bc0b6e856d8e847887
alejandrorm <alejandrorm@alejandrorm>
2022-05-31 22:53:10 +0200

    Metodo para añadir datos en la base de datos implementado.

--- Proyecto JDBC comics/src/Funcionamiento/Controller.java
+++ Proyecto JDBC comics/src/Funcionamiento/Controller.java
@@ -1,5 +1,9 @@
 package Funcionamiento;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import DBManager.DBManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,6 +16,9 @@ public class Controller {
 
 	private static final String DB_MSQ_CONN_OK = "CONEXION CORRECTA";
 	private static final String DB_MSQ_CONN_NO = "ERROR EN LA CONEXION";
+	
+	// Conexión a la base de datos
+	private static Connection conn;
 
 	@FXML
 	private TextField anioPublicacion;
@@ -77,8 +84,31 @@ public class Controller {
 		nom_variante = nombreVariante.getText();
 		num_comic = numeroComic.getText();
 		
-		labelResultado.setText(nom_comic + " " + nom_dibujante);
-
+		try {
+			PreparedStatement statement = conn.prepareStatement(sentenciaSQL);
+			statement.setString(1, nom_comic);
+			statement.setString(2, nom_dibujante);
+			statement.setString(3, nom_editorial);
+			statement.setString(4, nom_guionista);
+			statement.setString(5, nom_variante);
+			statement.setString(6, num_comic);
+			statement.executeUpdate();
+			statement.close();
+			if(statement.executeUpdate() == 1)
+			{
+				labelConexion.setStyle("-fx-background-color: #A0F52D");
+				labelResultado.setText("Comic añadido correctamente!" + "\nNombre del comic: " + nom_comic 
+						+ "\nNombre del dibujante: " + nom_dibujante + "\nEditorial: " + nom_editorial 
+						+ "\nGuinista: " + "\nVariante: " + nom_variante + "\nNumero del comic: " + num_comic);
+			}
+			else
+			{
+				labelConexion.setStyle("-fx-background-color: #DD370F");
+				labelResultado.setText("Se ha encontrado un error. No ha sido posible añadir el comic a la base de datos.");
+			}
+		} catch (SQLException ex) {
+			System.err.println("Error al insertar un Cliente ");
+		}
 	}
 
 	@FXML
@@ -90,7 +120,8 @@ public class Controller {
 		{   
 			labelConexion.setStyle("-fx-background-color: #A0F52D");
 			labelConexion.setText("Cargando Driver... " + "OK! \nConectando a la base de datos... \n" + DB_MSQ_CONN_OK);
-			
+			// Conexión a la base de datos
+			conn = DBManager.conexion();
 		}
 		else
 		{
